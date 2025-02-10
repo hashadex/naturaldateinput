df7242c0c255b42e1a67651068dc163df49db0e9
hashadex <hashadex@outlook.com>
2025-02-10 23:20:46 +0300

    Rename hashmap `weekdayNameMap` to `weekdayNames`

--- src/main/java/me/hashadex/naturaldateinput/parsers/en/ENWeekdayDateParser.java
+++ src/main/java/me/hashadex/naturaldateinput/parsers/en/ENWeekdayDateParser.java
@@ -12,29 +12,29 @@ import me.hashadex.naturaldateinput.ParseResult;
 import me.hashadex.naturaldateinput.parsers.DateParser;
 
 public class ENWeekdayDateParser extends DateParser {
-    private static HashMap<String, DayOfWeek> weekdayNameMap = new HashMap<>();
+    private static HashMap<String, DayOfWeek> weekdayNames = new HashMap<>();
 
     static {
         // Monday
-        weekdayNameMap.put("monday", DayOfWeek.MONDAY);
-        weekdayNameMap.put("mon", DayOfWeek.MONDAY);
+        weekdayNames.put("monday", DayOfWeek.MONDAY);
+        weekdayNames.put("mon", DayOfWeek.MONDAY);
         // Tuesday
-        weekdayNameMap.put("tuesday", DayOfWeek.TUESDAY);
-        weekdayNameMap.put("tue", DayOfWeek.TUESDAY);
+        weekdayNames.put("tuesday", DayOfWeek.TUESDAY);
+        weekdayNames.put("tue", DayOfWeek.TUESDAY);
         // Wednesday
-        weekdayNameMap.put("wednesday", DayOfWeek.WEDNESDAY);
-        weekdayNameMap.put("wed", DayOfWeek.WEDNESDAY);
+        weekdayNames.put("wednesday", DayOfWeek.WEDNESDAY);
+        weekdayNames.put("wed", DayOfWeek.WEDNESDAY);
         // Thursday
-        weekdayNameMap.put("thursday", DayOfWeek.THURSDAY);
-        weekdayNameMap.put("thu", DayOfWeek.THURSDAY);
+        weekdayNames.put("thursday", DayOfWeek.THURSDAY);
+        weekdayNames.put("thu", DayOfWeek.THURSDAY);
         // Friday
-        weekdayNameMap.put("friday", DayOfWeek.FRIDAY);
-        weekdayNameMap.put("fri", DayOfWeek.FRIDAY);
+        weekdayNames.put("friday", DayOfWeek.FRIDAY);
+        weekdayNames.put("fri", DayOfWeek.FRIDAY);
         // Saturday
-        weekdayNameMap.put("saturday", DayOfWeek.SATURDAY);
-        weekdayNameMap.put("sat", DayOfWeek.SATURDAY);
+        weekdayNames.put("saturday", DayOfWeek.SATURDAY);
+        weekdayNames.put("sat", DayOfWeek.SATURDAY);
         // Sunday
-        weekdayNameMap.put("sunday", DayOfWeek.SUNDAY);
+        weekdayNames.put("sunday", DayOfWeek.SUNDAY);
         // "sun" is not in the map to avoid confusion with the Sun (the star)
     }
 
@@ -43,7 +43,7 @@ public class ENWeekdayDateParser extends DateParser {
             "\\b" +                                                                 // word boundary
             "(?:on |the ){0,2}" +                                                   // optionally match "on the"
             "(?:(?<modifier>next) )?" +                                             // next modifier
-            "(?<weekday>" + keySetToRegexAlternate(weekdayNameMap.keySet()) + ")" + // weekday
+            "(?<weekday>" + keySetToRegexAlternate(weekdayNames.keySet()) + ")" + // weekday
             "\\b"                                                                   // word boundary
         );
     }
@@ -60,7 +60,7 @@ public class ENWeekdayDateParser extends DateParser {
                 nextModifier = true;
             }
 
-            DayOfWeek selected = weekdayNameMap.get(match.group("weekday").toLowerCase());
+            DayOfWeek selected = weekdayNames.get(match.group("weekday").toLowerCase());
 
             LocalDate result = reference.toLocalDate().with(
                 TemporalAdjusters.nextOrSame(selected)
