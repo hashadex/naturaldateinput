378df84560524eb7eb2192f0c885291653749593
hashadex <hashadex@outlook.com>
2025-02-17 19:11:40 +0300

    Use hashmap instead of switch statement

--- src/main/java/me/hashadex/naturaldateinput/parsers/en/ENTimeUnitLaterDateParser.java
+++ src/main/java/me/hashadex/naturaldateinput/parsers/en/ENTimeUnitLaterDateParser.java
@@ -2,21 +2,33 @@ package me.hashadex.naturaldateinput.parsers.en;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.regex.MatchResult;
 
 import me.hashadex.naturaldateinput.ParseResult;
 import me.hashadex.naturaldateinput.parsers.DateParser;
 
 public class ENTimeUnitLaterDateParser extends DateParser {
+    private static HashMap<String, ChronoUnit> chronoUnits = new HashMap<>();
+    
+    static {
+        chronoUnits.put("day", ChronoUnit.DAYS);
+        chronoUnits.put("week", ChronoUnit.WEEKS);
+        chronoUnits.put("month", ChronoUnit.MONTHS);
+        chronoUnits.put("year", ChronoUnit.YEARS);
+    }
+
     public ENTimeUnitLaterDateParser() {
         //\b(?:in |after )?(\d+) (day|week|month|year)s?(?: later| after| from now| henceforth |forward |out)?\b
         super(
-            "\\b" +                                                    // word boundary
-            "(?:in |after )?" +                                        // optionally match "in" or "after"
-            "(?<amount>\\d+) (?<timeunit>day|week|month|year)s?" +     // amount + time unit
-            "(?: later| after| from now)?" +                           // optionally match later, after, etc.
-            "\\b"                                                      // word boundary
+            "\\b" +                                                                 // word boundary
+            "(?:in |after )?" +                                                     // optionally match "in" or "after"
+            "(?<amount>\\d+) " +                                                    // amount + space
+            "(?<timeunit>" + keySetToRegexAlternate(chronoUnits.keySet()) + ")s?" + // timeunit
+            "(?: later| after| from now)?" +                                        // optionally match later, after, etc.
+            "\\b"                                                                   // word boundary
         );
     }
 
@@ -26,28 +38,10 @@ public class ENTimeUnitLaterDateParser extends DateParser {
         ArrayList<MatchResult> matches = findAllMatches(input);
 
         for (MatchResult match : matches) {
-            int amount = Integer.parseInt(match.group("amount"));
-
-            LocalDate referenceDate = reference.toLocalDate();
-            LocalDate result;
-
-            switch (match.group("timeunit")) {
-                case "day":
-                    result = referenceDate.plusDays(amount);
-                    break;
-                case "week":
-                    result = referenceDate.plusWeeks(amount);
-                    break;
-                case "month":
-                    result = referenceDate.plusMonths(amount);
-                    break;
-                case "year":
-                    result = referenceDate.plusYears(amount);
-                    break;
-            
-                default:
-                    continue;
-            }
+            LocalDate result = reference.toLocalDate().plus(
+                Integer.parseInt(match.group("amount")),
+                chronoUnits.get(match.group("timeunit"))
+            );
 
             results.add(
                 new ParseResult<LocalDate>(reference, input, match, result)
