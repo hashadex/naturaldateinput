ceba5b9874f1844a3e23b9462defc20e748fbe43
hashadex <hashadex@outlook.com>
2025-02-23 22:02:35 +0300

    Move utility methods to `Parser`

--- src/main/java/me/hashadex/naturaldateinput/parsers/DateParser.java
+++ src/main/java/me/hashadex/naturaldateinput/parsers/DateParser.java
@@ -2,9 +2,7 @@ package me.hashadex.naturaldateinput.parsers;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.ArrayList;
-import java.util.Set;
 
 import me.hashadex.naturaldateinput.ParseResult;
 
@@ -15,40 +13,6 @@ public abstract class DateParser extends Parser {
 
     public DateParser(String regex) {
         super(regex);
-    }
-
-    protected static boolean isWithinYearRange(int year) {
-        return year >= 1900 && year <= 2200; // This is the range supported by Todoist
-    }
-
-    protected static boolean isWithinMonthRange(int month) {
-        return month >= 1 && month <= 12;
-    }
-
-    protected static boolean isWithinDayRange(int day) {
-        return day >= 1 && day <= 31;
-    }
-
-    protected static int normalizeYear(int year) {
-        year = Math.abs(year);
-
-        if (year < 100) {
-            return 2000 + year;
-        } else {
-            return year;
-        }
-    }
-
-    protected static LocalDate safeGetLocalDate(int year, int month, int day) {
-        return LocalDate.of(normalizeYear(year), month, 1).plusDays(day - 1);
-    }
-
-    protected static LocalDate safeGetLocalDate(int year, Month month, int day) {
-        return safeGetLocalDate(year, month.getValue(), day);
-    }
-
-    protected static String keySetToRegexAlternate(Set<String> set) {
-        return String.join("|", set);
     }
 
     public abstract ArrayList<ParseResult<LocalDate>> parse(String input, LocalDateTime reference);
--- src/main/java/me/hashadex/naturaldateinput/parsers/Parser.java
+++ src/main/java/me/hashadex/naturaldateinput/parsers/Parser.java
@@ -1,6 +1,9 @@
 package me.hashadex.naturaldateinput.parsers;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,5 +28,39 @@ public abstract class Parser {
         }
 
         return matchList;
+    }
+
+    protected static boolean isWithinYearRange(int year) {
+        return year >= 1900 && year <= 2200; // This is the range supported by Todoist
+    }
+
+    protected static boolean isWithinMonthRange(int month) {
+        return month >= 1 && month <= 12;
+    }
+
+    protected static boolean isWithinDayRange(int day) {
+        return day >= 1 && day <= 31;
+    }
+
+    protected static int normalizeYear(int year) {
+        year = Math.abs(year);
+
+        if (year < 100) {
+            return 2000 + year;
+        } else {
+            return year;
+        }
+    }
+
+    protected static LocalDate safeGetLocalDate(int year, int month, int day) {
+        return LocalDate.of(normalizeYear(year), month, 1).plusDays(day - 1);
+    }
+
+    protected static LocalDate safeGetLocalDate(int year, Month month, int day) {
+        return safeGetLocalDate(year, month.getValue(), day);
+    }
+
+    protected static String keySetToRegexAlternate(Set<String> set) {
+        return String.join("|", set);
     }
 }
