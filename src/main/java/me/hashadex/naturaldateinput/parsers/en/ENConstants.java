460c0b3f6e5da0ebdeab407b85c7b54929ceaf4e
hasha <hashadex@outlook.com>
2025-06-18 15:42:15 +0300

    Don't use static blocks to initialize fields

--- src/main/java/me/hashadex/naturaldateinput/parsers/en/ENConstants.java
+++ src/main/java/me/hashadex/naturaldateinput/parsers/en/ENConstants.java
@@ -40,113 +40,105 @@ import java.util.Map;
 import java.util.Set;
 
 class ENConstants {
-    public static final Map<String, Month> monthMap;
-    public static final Map<String, Integer> relativeWordOffsetMap;
-    public static final Map<String, ChronoUnit> chronoUnitMap;
-    public static final Map<String, DayOfWeek> weekdayMap;
-    public static final Set<String> ordinalIndicators;
+    public static final Map<String, Month> monthMap = Map.ofEntries(
+        // January
+        entry("january", JANUARY),
+        entry("jan.", JANUARY),
+        entry("jan", JANUARY),
+        // February
+        entry("february", FEBRUARY),
+        entry("feb.", FEBRUARY),
+        entry("feb", FEBRUARY),
+        // March
+        entry("march", MARCH),
+        entry("mar.", MARCH),
+        entry("mar", MARCH),
+        // April
+        entry("april", APRIL),
+        entry("apr.", APRIL),
+        entry("apr", APRIL),
+        // May
+        entry("may", MAY),
+        entry("may.", MAY),
+        // June
+        entry("june", JUNE),
+        entry("jun.", JUNE),
+        entry("jun", JUNE),
+        // July
+        entry("july", JULY),
+        entry("jul.", JULY),
+        entry("jul", JULY),
+        // August
+        entry("august", AUGUST),
+        entry("aug.", AUGUST),
+        entry("aug", AUGUST),
+        // September
+        entry("september", SEPTEMBER),
+        entry("sep.", SEPTEMBER),
+        entry("sep", SEPTEMBER),
+        // October
+        entry("october", OCTOBER),
+        entry("oct.", OCTOBER),
+        entry("oct", OCTOBER),
+        // November
+        entry("november", NOVEMBER),
+        entry("nov.", NOVEMBER),
+        entry("nov", NOVEMBER),
+        // December
+        entry("december", DECEMBER),
+        entry("dec.", DECEMBER),
+        entry("dec", DECEMBER)
+    );
 
-    static {
-        monthMap = Map.ofEntries(
-            // January
-            entry("january", JANUARY),
-            entry("jan.", JANUARY),
-            entry("jan", JANUARY),
-            // February
-            entry("february", FEBRUARY),
-            entry("feb.", FEBRUARY),
-            entry("feb", FEBRUARY),
-            // March
-            entry("march", MARCH),
-            entry("mar.", MARCH),
-            entry("mar", MARCH),
-            // April
-            entry("april", APRIL),
-            entry("apr.", APRIL),
-            entry("apr", APRIL),
-            // May
-            entry("may", MAY),
-            entry("may.", MAY),
-            // June
-            entry("june", JUNE),
-            entry("jun.", JUNE),
-            entry("jun", JUNE),
-            // July
-            entry("july", JULY),
-            entry("jul.", JULY),
-            entry("jul", JULY),
-            // August
-            entry("august", AUGUST),
-            entry("aug.", AUGUST),
-            entry("aug", AUGUST),
-            // September
-            entry("september", SEPTEMBER),
-            entry("sep.", SEPTEMBER),
-            entry("sep", SEPTEMBER),
-            // October
-            entry("october", OCTOBER),
-            entry("oct.", OCTOBER),
-            entry("oct", OCTOBER),
-            // November
-            entry("november", NOVEMBER),
-            entry("nov.", NOVEMBER),
-            entry("nov", NOVEMBER),
-            // December
-            entry("december", DECEMBER),
-            entry("dec.", DECEMBER),
-            entry("dec", DECEMBER)
-        );
+    public static final Map<String, Integer> relativeWordOffsetMap = Map.of(
+        // yesterday
+        "yesterday", -1,
+        "ytd", -1,
+        // today
+        "today", 0,
+        "tod", 0,
+        // tomorrow
+        "tomorrow", 1,
+        "tmrw", 1,
+        "tmr", 1
+    );
 
-        relativeWordOffsetMap = Map.of(
-            // yesterday
-            "yesterday", -1,
-            "ytd", -1,
-            // today
-            "today", 0,
-            "tod", 0,
-            // tomorrow
-            "tomorrow", 1,
-            "tmrw", 1,
-            "tmr", 1
-        );
-
-        chronoUnitMap = Map.of(
-            "decade", DECADES,
-            "year", YEARS,
-            "month", MONTHS,
-            "week", WEEKS,
-            "day", DAYS,
-            "half-day", HALF_DAYS,
-            "halfday", HALF_DAYS,
-            "hour", HOURS,
-            "minute", MINUTES,
-            "seconds", SECONDS
-        );
-
-        weekdayMap = Map.ofEntries(
-            // Monday
-            entry("monday", MONDAY),
-            entry("mon", MONDAY),
-            // Tuesday
-            entry("tuesday", TUESDAY),
-            entry("tue", TUESDAY),
-            // Wednesday
-            entry("wednesday", WEDNESDAY),
-            entry("wed", WEDNESDAY),
-            // Thursday
-            entry("thursday", THURSDAY),
-            entry("thu", THURSDAY),
-            // Friday
-            entry("friday", FRIDAY),
-            entry("fri", FRIDAY),
-            // Saturday
-            entry("saturday", SATURDAY),
-            entry("sat", SATURDAY),
-            // Sunday
-            entry("sunday", SUNDAY)
-            // "sun" is not in the map to avoid confusion with the Sun (the star)
-        );
-
-        ordinalIndicators = Set.of("th", "st", "nd", "rd");
-    }
+    public static final Map<String, ChronoUnit> chronoUnitMap = Map.of(
+        "decade", DECADES,
+        "year", YEARS,
+        "month", MONTHS,
+        "week", WEEKS,
+        "day", DAYS,
+        "half-day", HALF_DAYS,
+        "halfday", HALF_DAYS,
+        "hour", HOURS,
+        "minute", MINUTES,
+        "seconds", SECONDS
+    );
+    
+    public static final Map<String, DayOfWeek> weekdayMap = Map.ofEntries(
+        // Monday
+        entry("monday", MONDAY),
+        entry("mon", MONDAY),
+        // Tuesday
+        entry("tuesday", TUESDAY),
+        entry("tue", TUESDAY),
+        // Wednesday
+        entry("wednesday", WEDNESDAY),
+        entry("wed", WEDNESDAY),
+        // Thursday
+        entry("thursday", THURSDAY),
+        entry("thu", THURSDAY),
+        // Friday
+        entry("friday", FRIDAY),
+        entry("fri", FRIDAY),
+        // Saturday
+        entry("saturday", SATURDAY),
+        entry("sat", SATURDAY),
+        // Sunday
+        entry("sunday", SUNDAY)
+        // "sun" is not in the map to avoid confusion with the Sun (the star)
+    );
+    
+    public static final Set<String> ordinalIndicators = Set.of("th", "st", "nd", "rd");
 }
